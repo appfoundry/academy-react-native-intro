756bc7a8fef925a945050131f3d348842c937847
M <ict.debaere@gmail.com>
2017-10-25 16:30:52 +0200

    Added logic to take a new picture on avatar press

--- containers/ProfileScreen.js
+++ containers/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Text } from 'react-native'
+import { ImagePicker } from 'expo'
 
 import ViewContainer from './ViewContainer'
 
@@ -7,12 +8,26 @@ import MainButton from '../components/MainButton'
 import AvatarImage from '../components/AvatarImage'
 
 class ProfileScreen extends Component {
+  constructor(props) {
+    super(props)
+
+    // this will reset every time the profile screen is used,
+    // if we would want to keep it, we'd have to implement some kind of storage (for example: React Native's AsyncStorage)
+    this.state = {
+      avatarSource: null
+    }
+  }
+
   render() {
     const {navigation} = this.props
+    const { avatarSource } = this.state
+    // check if a new avatar has been chosen, if not, use placeholder from assets
+    const avatarToShow = avatarSource ? { uri: avatarSource } : require('../assets/images/avatar.jpg')
+
     return (
       <ViewContainer>
         <AvatarImage onAvatarPress={ this._takeNewPicture }
-                     source={ require('../assets/images/avatar.jpg') } />
+                     source={avatarToShow} />
         <Text>
           { `Profile Screen of ${navigation.state.params.name}.` }
         </Text>
@@ -20,6 +35,21 @@ class ProfileScreen extends Component {
                     label="Go back!" />
       </ViewContainer>
     )
+  }
+  
+  _takeNewPicture = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    })
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({
+        avatarSource: result.uri
+      })
+    }
   }
 
   _buttonPress = () => {
